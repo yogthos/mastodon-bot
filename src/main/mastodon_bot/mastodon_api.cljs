2f37e679d3c4bb6e8129b0da2772e32bf82844dc
jem <michael.jerger@meissa-gmbh.de>
2020-05-22 17:59:42 +0200

    add some todos

--- src/main/mastodon_bot/mastodon_api.cljs
+++ src/main/mastodon_bot/mastodon_api.cljs
@@ -74,6 +74,7 @@
    status-id string?]
   (.delete (mastodon-client mastodon-config) (str "statuses/" status-id) #js {}))
 
+;; TODO: move to twitter
 (defn resolve-url [[uri]]
   (try
     (or
@@ -87,8 +88,10 @@
      uri)
     (catch js/Error _ uri)))
 
+;; TODO: move to twitter
 (def shortened-url-pattern #"(https?://)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?")
 
+;; TODO: move to twitter
 (defn-spec resolve-urls string?
   [mastodon-config mastodon-config?
    text string?]
